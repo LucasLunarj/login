5ae0e77b6e5cf6fc011d9717c1bc37a5433a4370
LucasLunarj <87677530+LucasLunarj@users.noreply.github.com>
2023-07-17 01:05:08 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+
+export function App(){
+  return(
+    <div className='w-screen h-screen p-5 bg-slate-800  gap-4 flex-col items-center'>
+     <div className="items-center">
+       <h1 className="from-neutral-100 text-slate-100 ">Sistema de Login</h1>
+           <div className="flex-col items-center ">
+        <div>
+          <input className="bg-slate-700 rounded-md w-1/3 h-8" type="email" name="" id="" />
+          </div>
+          <input className="bg-slate-700 rounded-md w-1/3 h-8" type="password" name="" id="" />
+           </div>
+     </div>
+    </div>
+  )
+}
