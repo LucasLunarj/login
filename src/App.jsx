80636734f15a4978a5062743f66d30b413312dcc
LucasLunarj <87677530+LucasLunarj@users.noreply.github.com>
2023-07-17 19:20:37 -0300

    new edits

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,21 @@
 
 export function App(){
   return(
-    <div className='w-screen h-screen p-5 bg-slate-800  gap-4 flex-col items-center'>
-     <div className="items-center">
-       <h1 className="from-neutral-100 text-slate-100 ">Sistema de Login</h1>
-           <div className="flex-col items-center ">
+    <div className='h-screen p-5 bg-slate-800 align-middle '>
+     <div className="flex-col border items-end">
+       <h1 className="from-neutral-100 text-slate-100 text-lg bold ">Sistema de Login</h1>
+           <div>
         <div>
+          <label htmlFor="">Endereço de Email</label>
           <input className="bg-slate-700 rounded-md w-1/3 h-8" type="email" name="" id="" />
           </div>
-          <input className="bg-slate-700 rounded-md w-1/3 h-8" type="password" name="" id="" />
+          <div className="flex-col">
+            <label htmlFor="">Senha</label>
+            <input className="bg-slate-700 rounded-md w-1/3 h-8" type="password" name="" id="" />
+          </div>
            </div>
+           <button>Entrar</button>
+           <p>não é membro?<a className="text-yellow-400" href="#">Faça seu login agora</a></p>
      </div>
     </div>
   )
